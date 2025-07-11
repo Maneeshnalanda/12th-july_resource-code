be26f970fb9d7b80b65cc54de75f62117c77fb2c
Maneeshnalanda <maneesh202@gmail.com>
2025-07-11 16:54:19 +0530

    my lates code

--- main.tf
+++ main.tf
@@ -18,3 +18,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example-resources"
+  location = "West Europe"
+}
